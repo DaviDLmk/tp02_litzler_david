20fb0d360b46f2f68dc4dd04dcb42234fdf761be
DaviDLmk <litzlerdavid@gmail.com>
2020-09-24 17:06:23 +0200

    Feat(app.component) : Initialisation et ajout des components header, form, footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { FormComponent } from './form/form.component';
+import { TeteComponent } from './tete/tete.component';
+import { FooterComponent } from './footer/footer.component';
+import { RecapComponent } from './recap/recap.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, FormComponent ],
+  declarations: [ AppComponent, HelloComponent, FormComponent, TeteComponent, FooterComponent, RecapComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
